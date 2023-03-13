c57fbfae8bd01224d51511b996956f8e1550c373
Conraldmonyenye <122484956+Conraldmonyenye@users.noreply.github.com>
2023-03-13 22:30:20 +0300

    Add files via upload

---
+++ create.dart
@@ -0,0 +1,11 @@
+const number = 10;
+String message='';
+ void main() {
+  if(number > 10){
+    message = 'Your number is greater than 10.';
+  } else if(number < 10){
+    message = 'Your number is less than 10.';
+  } else (number == 10){
+    message = 'Your number is equal to 10.';
+  };
+}
